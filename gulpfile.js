e8addd3a18af4e1891598fd96ef636722b4f618d
developer-do <dev-do@naver.com>
2018-11-06 00:34:51 +0900

    STEP 7. CSS Minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
 
 gulp.task('uglify', function(){
-    return gulp.src('src/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minifycss', function(){
+    return gulp.src('src/css/main.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('src/*.js', ['uglify']);
+    gulp.watch('src/**/*.js', ['uglify']);
+    gulp.watch('src/css/*.css', ['minifycss']);
 });
 
 
-gulp.task('default', ['uglify', 'watch']);
+gulp.task('default', ['uglify', 'minifycss', 'watch']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 function oh() {
     console.log("어렵디");
+    console.log("scss뭐지");
 }
