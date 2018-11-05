c86445d6a45cb275c93cf10c34882983a598c669
developer-do <dev-do@naver.com>
2018-11-06 00:56:30 +0900

    STEP 8. BrowserSync로 미ë니 서버를 띄워서 작업하기

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,55 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var minifycss = require('gulp-minify-css');
+var minifyhtml = require('gulp-minify-html');
+var browserSync = require('browser-sync').create();
 
-gulp.task('uglify', function(){
-    return gulp.src('src/**/*.js')
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+// dist 폴더를 기준으로 웹서버 실행
+gulp.task('server', ['uglify', 'minifycss', 'minifyhtml'], function () {
+    return browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
 });
 
+// HTML 파일을 minify
+gulp.task('minifyhtml', function () {
+    return gulp.src('src/**/*.html') // src 폴더 아래의 모든 html 파일을 
+        .pipe(minifyhtml()) // minify 해서
+        .pipe(gulp.dest('dist')) // dist 폴더에 저장
+        .pipe(browserSync.reload({
+            stream: true // browserSync 로 브라우저에 반영
+        }));
+})
+
+// javaScript 파일을 minify
+gulp.task('uglify', function(){
+    return gulp.src('src/**/*.js') // src 폴더 아래의 모든 html 파일을
+        .pipe(concat('main.js')) // 병합하고
+        .pipe(uglify()) // minify 해서
+        .pipe(gulp.dest('dist/js')) // dist 폴더에 저장
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// CSS 파일을 minify
 gulp.task('minifycss', function(){
-    return gulp.src('src/css/main.css')
-        .pipe(minifycss())
-        .pipe(gulp.dest('dist/css'));
+    return gulp.src('src/css/main.css') // src 폴더 아래의 모든 css 파일을
+        .pipe(concat('main.css')) // 병합하고
+        .pipe(minifycss()) // minify 해서
+        .pipe(gulp.dest('dist/css')) // dist 폴더에 저장
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('watch', function(){
     gulp.watch('src/**/*.js', ['uglify']);
     gulp.watch('src/css/*.css', ['minifycss']);
+    gulp.watch('src/**/*.html', ['minifyhtml']);
 });
 
 
-gulp.task('default', ['uglify', 'minifycss', 'watch']);
+gulp.task('default', ['server', 'watch']);
