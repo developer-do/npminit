c42d60884ae8cb26af0f2dec1ab26d25cf56a2e0
developer-do <dev-do@naver.com>
2018-11-05 22:56:27 +0900

    STEP 3. Series Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
 
 gulp.task('hello', function () {
-    return console.log('Hello World');
+    return console.log('Hello');
 });
 
-gulp.task('default', ['hello']);
+gulp.task('world', function () {
+    return console.log('World');
+});
+
+gulp.task('default', ['hello', 'world']);
